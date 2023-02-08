92f04d55e391e303cfbd4160e08e93637b80346e
Evaldas Melianas <101465099+psysheep@users.noreply.github.com>
2023-02-08 21:26:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Class.table_actions import ProjectCRUD
 
 crud = ProjectCRUD()
-crud.add_employee('Aurimas', 'Sprogimas', '1965-01-15', 'Team Lead', 3000)
+"""crud.add_employee('Aurimas', 'Sprogimas', '1965-01-15', 'Team Lead', 3000)
 crud.add_employee('Albertas', 'Suvis', '1990-10-15', 'Developer')
 crud.add_employee('John', 'Goodman', '1973-02-30', 'Team quality manager', 2500)
 crud.add_employee('Nic', 'Badman', '2000-05-10', 'Developer')
@@ -11,5 +11,5 @@ crud.add_employee('Elena', 'Sulcaite', '2001-01-11', 'Developer')
 crud.add_employee('Egle', 'Bartkeviciute', '2000-02-22', 'Developer')
 
 crud.change_salary(1, 5000)
-crud.fire_employee(7)
+crud.fire_employee(7)"""
 print(crud.find_active())
