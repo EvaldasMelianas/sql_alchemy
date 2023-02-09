cb522625e504ca4ce2b3cf38e62d26a3c3f9eec5
Evaldas Melianas <101465099+psysheep@users.noreply.github.com>
2023-02-09 22:54:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Class.table_actions import ProjectCRUD
 
 crud = ProjectCRUD()
-crud.add_employee([('Aurimas', 'Sprogimas', '1965-01-15', 'Team Lead', 3000),
+"""crud.add_employee([('Aurimas', 'Sprogimas', '1965-01-15', 'Team Lead', 3000),
                    ('Albertas', 'Suvis', '1990-10-15', 'Developer'),
                    ('John', 'Goodman', '1973-02-28', 'Team quality manager', 2500),
                    ('Nic', 'Badman', '2000-05-10', 'Developer'),
@@ -9,10 +9,10 @@ crud.add_employee([('Aurimas', 'Sprogimas', '1965-01-15', 'Team Lead', 3000),
                    ('Albertas', 'Jonauskas', '1989-01-10', 'Product specialist', 2500),
                    ('Sarunas', 'Aurimas', '1989-01-10', 'Product specialist', 2500),
                    ('Egle', 'Bartkeviciute', '1989-01-10', 'Product specialist', 2500),
-                   ('Elegija', 'Barstkeviciute', '1989-01-10', 'Product specialist', 2500)])
+                   ('Elegija', 'Barstkeviciute', '1989-01-10', 'Product specialist', 2500)])"""
 
-crud.change_salary(8, 3500)
-crud.fire_employee(7)
-print(crud.find_active(False))
+#crud.change_salary(8, 3500)
+#crud.fire_employee(7)
+#print(crud.find_active(False))
 print(crud.find_pay_above(1500))
-print(crud.find_by_fragment('Aur'))
+print(crud.find_by_fragment('Sar'))
